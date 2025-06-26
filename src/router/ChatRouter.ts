5b1af64aa699187b24ed883473d7058ac365790b
Leonardo Pesce <80352672+leopesce05@users.noreply.github.com>
2025-06-25 23:59:03 -0300

    fix

--- src/router/ChatRouter.ts
+++ src/router/ChatRouter.ts
@@ -9,7 +9,7 @@ const chatRouter = Router();
 
 chatRouter.post('/',
     auth,
-    body('name').notEmpty().withMessage('El nombre es requerido').isLength({min: 3, max: 16}).withMessage('El nombre debe tener entre 3 y 16 caracteres'),
+    body('name').optional().withMessage('El nombre es requerido').isLength({min: 3, max: 16}).withMessage('El nombre debe tener entre 3 y 16 caracteres'),
     body('type').isIn(['private', 'group']).withMessage('Los valores aceptables son private o group'),
     body('members').isArray().withMessage('Los miembros tienen que ser un arreglo'),
     body('members.*').isMongoId().withMessage('Los miembros tienen que ser un arreglo de ids de mongoDB'),

14efbf28211ed1a91c42b78733f7eeeda07aa304
Leonardo Pesce <80352672+leopesce05@users.noreply.github.com>
2025-06-27 21:58:48 -0300

    [FEAT] CORS CONFIGURATION

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,1 +1,20 @@
-//TODO: Implementar cors
+import {CorsOptions} from 'cors';
+import colors from 'colors';
+
+export const corsOptions: CorsOptions = {
+    origin: function(origin, callback){
+
+        const whiteList = [process.env.FRONTEND_URL]
+
+        if(process.env.NODE_ENV === 'LOCAL'){
+            whiteList.push(undefined)
+        }
+        
+        if (whiteList.includes(origin)){
+            callback(null, true);
+        }else{
+            console.log(origin);
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import router from './router';
 import dotenv from 'dotenv';
-import connectDB from './config/db';
 import morgan from 'morgan';
-import colors from 'colors';
+import cors from 'cors';
+
+import connectDB from './config/db';
+import {corsOptions} from './config/cors';
+import router from './router';
 
 // Configurar variables de entorno
 dotenv.config();
@@ -18,6 +20,9 @@ app.use(morgan('dev'));
 
 // Middleware for parsing JSON bodies
 app.use(express.json());
+
+// CORS
+app.use(cors(corsOptions));
 
 // Routes
 app.use(router);
